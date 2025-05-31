15329a05a4a8dfe2a4972e9656459bc03dbed804
Ziyan Ali Murtaza <ziyanali6@gmail.com>
2025-05-31 22:46:13 +0500

    signup added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
-import Menu from './components/Menu';
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
       <Menu />
       {/* Your other components/routes */}
     </BrowserRouter>
